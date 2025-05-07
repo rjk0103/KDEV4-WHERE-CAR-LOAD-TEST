260301df5865256c67e6495a392037e64f6e03aa
jaekyung <rkj0103@naver.com>
2025-05-07 15:33:21 +0900

    Trouble_Shooting: async 500TPS / 1sec / 3000TPS

--- src/gatling/java/mytest/GpsLoadSimulation.java
+++ src/gatling/java/mytest/GpsLoadSimulation.java
@@ -26,7 +26,7 @@ public class GpsLoadSimulation extends Simulation {
     {
         setUp(
                 scn.injectOpen(
-                        constantUsersPerSec(500).during(Duration.ofSeconds(6))
+                        constantUsersPerSec(500).during(Duration.ofSeconds(1))
                 )
         ).protocols(httpProtocol);
     }
