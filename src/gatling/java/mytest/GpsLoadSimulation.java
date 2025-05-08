21ce3328f57e13974930c7bd4c105eabfd993cf9
jaekyung <rkj0103@naver.com>
2025-05-08 12:00:06 +0900

    Trouble_Shooting: MQ 400TPS / 1sec / 2400TPS

--- src/gatling/java/mytest/GpsLoadSimulation.java
+++ src/gatling/java/mytest/GpsLoadSimulation.java
@@ -30,7 +30,7 @@ public class GpsLoadSimulation extends Simulation {
     {
         setUp(
                 scn.injectOpen(
-                        constantUsersPerSec(100).during(Duration.ofSeconds(1))
+                        constantUsersPerSec(400).during(Duration.ofSeconds(1))
                 )
         ).protocols(httpProtocol);
     }
